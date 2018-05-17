81006aef27cf7a42ce159a7ae6465c9f81dd8eda
warrenbrown <warren@katapultmedia.com>
2018-05-17 09:40:41 -0500

    added theme to site

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,16 @@ import { PostListComponent } from './components/post-list/post-list.component';
 import { PostDetailComponent } from './components/post-detail/post-detail.component';
 import { PostNewComponent } from './components/post-new/post-new.component';
 import { PostService } from './services/post.service';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
+import { SidebarComponent } from './shared/layout/sidebar/sidebar.component';
+import { ModuleWithProviders } from '@angular/core';
+
+import { DashboardModule } from './dashboard/dashboard.module';
+import { SharedModule } from './shared/shared.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true });
 
 
 @NgModule({
@@ -17,13 +28,19 @@ import { PostService } from './services/post.service';
     AppComponent,
     PostListComponent,
     PostDetailComponent,
-    PostNewComponent
+    PostNewComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    rootRouting,
     HttpModule,
-    FormsModule
+    FormsModule,
+    DashboardModule,
+    SharedModule
   ],
   providers: [PostService],
   bootstrap: [AppComponent]
