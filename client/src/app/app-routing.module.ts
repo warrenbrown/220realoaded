c3258340c3dafe714f72817c4370d41ae1cbb46a
warrenbrown <warren@katapultmedia.com>
2018-05-18 14:46:31 -0500

    added styling to the app

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { PostListComponent } from './components/post-list/post-list.component';
 import { PostDetailComponent } from './components/post-detail/post-detail.component';
 import { PostNewComponent } from './components/post-new/post-new.component';
 
+import { HomepageComponent } from './components/homepage/homepage.component';
+
 const routes: Routes = [
   { path: 'posts', component: PostListComponent },
   { path: 'post/:id', component: PostDetailComponent },
-  { path: 'posts/new', component: PostNewComponent }
+  { path: 'posts/new', component: PostNewComponent },
+  { path: 'home', component: HomepageComponent }
 
 ];
 
